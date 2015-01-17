97f408a436940170a9f0b7997ac759b195e1cbae
RoundTheCode <davidgrace00@gmail.com>
2015-01-17 14:48:32 +0000

    Import TagManagerExtensions test site.

---
+++ RoundTheCode.TagManagerExtensions.Test/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace RoundTheCode.TagManagerExtensions.Test
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
